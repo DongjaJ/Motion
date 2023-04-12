6450a9edc3074f265d076a979a819191249fbe0a
Dongja <jdh981118@naver.com>
2023-04-12 21:27:47 +0900

    feat: 모달에서 추가 버튼 누르면 메모 추가

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,10 @@
 import ModalImpl from './Modal.js';
+import { makeNoteTemplate } from './template/ListItem.js';
 interface TodoList {
 	setEvent(): void;
 }
+
+export type ContentType = 'Image' | 'Video' | 'Note' | 'Memo';
 
 class TodoListImpl implements TodoList {
 	constructor(private $target: HTMLElement, private modal: ModalImpl) {
@@ -11,12 +14,39 @@ class TodoListImpl implements TodoList {
 	setEvent() {
 		//버튼 클릭했을 때 모달창 activate
 		this.$target.addEventListener('click', (e) => {
-			const target = e.target;
-			if (!(target instanceof HTMLButtonElement)) return;
+			this.modalAddEvent(e);
+			const target = e.target as HTMLElement;
+			const buttonContainer = target.closest('.button__container');
+			if (!(target instanceof HTMLButtonElement && buttonContainer))
+				return;
 			const contentType = target.id;
 			this.modal.setContentType(contentType);
 			this.modal.toggleModal();
 		});
+	}
+
+	modalAddEvent(e: Event) {
+		const target = e.target as HTMLElement;
+		const addButton = target.closest('.add-item-btn');
+		if (!addButton) return;
+
+		const title = this.$target.querySelector(
+			`input[name="title"]`
+		) as HTMLInputElement;
+		const content = this.$target.querySelector(
+			'input[name="content"]'
+		) as HTMLInputElement;
+		const titleValue = title.value;
+		const contentValue = content.value;
+
+		const contentTemplate = makeNoteTemplate({
+			title: titleValue,
+			content: contentValue,
+		});
+
+		const todoListItems = this.$target.querySelector('.todo__list__items');
+		todoListItems?.insertAdjacentHTML('beforeend', contentTemplate);
+		this.modal.toggleModal();
 	}
 }
 
