a0821dfd470d3d68cdfaacafb556db6820d9cab7
Dongja <jdh981118@naver.com>
2023-04-12 22:10:16 +0900

    feat: 버튼 종류에 따라 누르면 아이템 다르게 생성

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,10 @@
 import ModalImpl from './Modal.js';
-import { makeNoteTemplate } from './template/ListItem.js';
+import {
+	makeImageTemplate,
+	makeVideoTemplate,
+	makeMemoTemplate,
+	makeNoteTemplate,
+} from './template/ListItem.js';
 interface TodoList {
 	setEvent(): void;
 }
@@ -16,14 +21,17 @@ class TodoListImpl implements TodoList {
 		this.$target.addEventListener('click', (e) => {
 			this.modalAddEvent(e);
 			this.deleteItemEvent(e);
-			const target = e.target as HTMLElement;
-			const buttonContainer = target.closest('.button__container');
-			if (!(target instanceof HTMLButtonElement && buttonContainer))
-				return;
-			const contentType = target.id;
-			this.modal.setContentType(contentType);
-			this.modal.toggleModal();
+			this.addEvent(e);
 		});
+	}
+
+	addEvent(e: Event) {
+		const target = e.target as HTMLElement;
+		const buttonContainer = target.closest('.button__container');
+		if (!(target instanceof HTMLButtonElement && buttonContainer)) return;
+		const contentType = target.id as ContentType;
+		this.modal.setContentType(contentType);
+		this.modal.toggleModal();
 	}
 
 	modalAddEvent(e: Event) {
@@ -40,7 +48,13 @@ class TodoListImpl implements TodoList {
 		const titleValue = title.value;
 		const contentValue = content.value;
 
-		const contentTemplate = makeNoteTemplate({
+		const contentTemplate = this.makeItemTemplate({
+			contentType: this.modal.contentType,
+			title: titleValue,
+			content: contentValue,
+		});
+
+		makeNoteTemplate({
 			title: titleValue,
 			content: contentValue,
 		});
@@ -48,6 +62,23 @@ class TodoListImpl implements TodoList {
 		const todoListItems = this.$target.querySelector('.todo__list__items');
 		todoListItems?.insertAdjacentHTML('beforeend', contentTemplate);
 		this.modal.toggleModal();
+	}
+
+	makeItemTemplate({
+		contentType,
+		title,
+		content,
+	}: {
+		contentType?: ContentType;
+		title: string;
+		content: string;
+	}) {
+		if (contentType === 'Image')
+			return makeImageTemplate({ title, content });
+		if (contentType === 'Video')
+			return makeVideoTemplate({ title, content });
+		if (contentType === 'Note') return makeNoteTemplate({ title, content });
+		return makeMemoTemplate({ title, content });
 	}
 
 	deleteItemEvent(e: Event) {
--- frontend/src/template/ListItem.ts
+++ frontend/src/template/ListItem.ts
@@ -1,5 +1,3 @@
-import { ContentType } from '../main';
-
 type TemplateParam = {
 	title: string;
 	content: String;
@@ -14,6 +12,53 @@ export function makeNoteTemplate({
 		<div class="item_content">
 			<div class="title">${title}</div>
 			<div class="content">${content}</div>
+		</div>
+		<div class="delete_button">x</div>
+	</div>`;
+}
+
+export function makeImageTemplate({
+	title,
+	content,
+}: Partial<TemplateParam>): string {
+	return `
+	<div class="todo__list__item Image">
+		<div class="item_content">
+			<div class="content">
+				<img src="./assets/background_1d7e7d9e57.png" />
+			</div>
+			<div class="title">${title}</div>
+		</div>
+		<div class="delete_button">x</div>
+	</div>`;
+}
+
+export function makeVideoTemplate({
+	title,
+	content,
+}: Partial<TemplateParam>): string {
+	return `
+	<div class="todo__list__item Video">
+		<div class="item_content">
+			<div class="content">
+				<video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+								controls></video>
+			</div>
+			<div class="title">${title}</div>
+		</div>
+		<div class="delete_button">x</div>
+	</div>`;
+}
+
+export function makeMemoTemplate({
+	title,
+	content,
+}: Partial<TemplateParam>): string {
+	return `
+	<div class="todo__list__item Memo">
+		<div class="item_content">
+			<div class="title">${title}</div>
+			<div class="content">● ${content}</div>
 		</div>
 		<div class="delete_button">x</div>
 	</div>`;
