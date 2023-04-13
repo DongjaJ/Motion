489caa347c663dd1ae5c324e3e0952e68d653081
Dongja <jdh981118@naver.com>
2023-04-13 19:15:20 +0900

    feat: 드래그 시 잔상이 마우스 따라 이동

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,6 +11,41 @@ interface TodoList {
 
 export type ContentType = 'Image' | 'Video' | 'Note' | 'Memo';
 
+const checkDropable = (e: MouseEvent, copy_item: HTMLElement) => {
+	copy_item.hidden = true;
+	let elemBelow = document.elementFromPoint(e.clientX, e.clientY);
+	copy_item.hidden = false;
+	if (!elemBelow) return;
+	return elemBelow;
+};
+
+function getDragAfterElement(container: HTMLElement, y: number) {
+	const draggableElements = [
+		...container.querySelectorAll('.todolist-items'),
+	];
+	const dragAfterElement: { offset: number; element?: HTMLElement } =
+		draggableElements.reduce(
+			(closest, child) => {
+				const box = child.getBoundingClientRect();
+				const offset = y - box.top - box.height / 2;
+				if (offset < 0 && offset > closest.offset) {
+					return { offset: offset, element: child };
+				} else {
+					return closest;
+				}
+			},
+			{ offset: Number.NEGATIVE_INFINITY }
+		);
+
+	return dragAfterElement?.element;
+}
+
+const getShift = (e: MouseEvent, origin_item: HTMLElement) => {
+	const shiftX = e.clientX - origin_item.getBoundingClientRect().left;
+	const shiftY = e.clientY - origin_item.getBoundingClientRect().top;
+	return { shiftX, shiftY };
+};
+
 class TodoListImpl implements TodoList {
 	constructor(private $target: HTMLElement, private modal: ModalImpl) {
 		this.setEvent();
@@ -22,6 +57,72 @@ class TodoListImpl implements TodoList {
 			this.modalAddEvent(e);
 			this.deleteItemEvent(e);
 			this.addEvent(e);
+		});
+
+		function dragStart() {
+			// copyItem.classList.toggle('dragging');
+			// todoList.append(copyItem);
+			// originItem.classList.toggle('dragged');
+		}
+
+		function excuteDrag(e: MouseEvent, todoList: HTMLElement) {
+			const target = e.target as HTMLElement;
+			const originItem = target.closest(
+				'.todo__list__item'
+			) as HTMLElement;
+			if (!originItem) return;
+
+			console.log('dragStart');
+			const { shiftX, shiftY } = getShift(e, originItem);
+			const copyItem = originItem.cloneNode(true) as HTMLElement;
+
+			copyItem.classList.toggle('dragging');
+			todoList.append(copyItem);
+			originItem.classList.toggle('dragged');
+
+			function moveAt(pageX: number, pageY: number) {
+				copyItem.style.left = pageX - shiftX + 'px';
+				copyItem.style.top = pageY - shiftY + 'px';
+			}
+
+			function onMouseMove(e: MouseEvent) {
+				moveAt(e.pageX, e.pageY);
+				const todolist = checkDropable(e, copyItem)?.closest(
+					'.todo__list__items'
+				);
+				if (!(todolist instanceof HTMLElement)) return;
+				const afterElement = getDragAfterElement(todolist, e.clientY);
+				if (afterElement === originItem) return;
+				// if (!afterElement) todolist.appendChild(originItem);
+				// todolist_ul.insertBefore(origin_item, afterElement);
+			}
+
+			function dragEndEvent() {
+				document.removeEventListener('mousemove', onMouseMove);
+				copyItem.remove();
+				originItem.classList.toggle('dragged');
+			}
+
+			//mouse 이동시 이벤트
+			document.onmousemove = onMouseMove;
+
+			document.onmouseup = dragEndEvent;
+		}
+
+		this.$target.addEventListener('mousedown', (e) => {
+			//drag start
+			const drag_delay = 150;
+			let isClick = 1;
+			const timer = setTimeout(() => {
+				if (!isClick) return;
+				isClick = 0;
+				excuteDrag(e, this.$target);
+			}, drag_delay);
+
+			document.onmouseup = () => {
+				clearTimeout(timer);
+				isClick = 0;
+			};
 		});
 	}
 
