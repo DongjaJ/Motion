9bf6598c667124df8cae6bd60e54000156cc202a
Dongja <jdh981118@naver.com>
2023-04-20 17:46:59 +0900

    feat: add item container

--- frontend_refactoring/src/app.ts
+++ frontend_refactoring/src/app.ts
@@ -5,21 +5,21 @@ import { NoteComponent } from './components/pages/item/NoteComponent.js';
 import { TodoComponent } from './components/pages/item/TodoComponent.js';
 class App {
 	private readonly page: PageComponent;
-	private readonly image: ImageComponent;
-	private readonly video: VideoComponent;
-	private readonly note: NoteComponent;
-	private readonly todo: TodoComponent;
 	constructor(appRoot: HTMLElement) {
 		this.page = new PageComponent();
 		this.page.attachTo(appRoot);
-		this.image = new ImageComponent();
-		this.image.attachTo(appRoot, 'beforeend');
-		this.video = new VideoComponent();
-		this.video.attachTo(appRoot, 'beforeend');
-		this.note = new NoteComponent();
-		this.note.attachTo(appRoot, 'beforeend');
-		this.todo = new TodoComponent();
-		this.todo.attachTo(appRoot, 'beforeend');
+
+		const image = new ImageComponent();
+		this.page.addChild(image);
+
+		const video = new VideoComponent();
+		this.page.addChild(video);
+
+		const note = new NoteComponent();
+		this.page.addChild(note);
+
+		const todo = new TodoComponent();
+		this.page.addChild(todo);
 	}
 }
 
